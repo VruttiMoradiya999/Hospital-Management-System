20ff5683669cc4ac9b3da89775f43d8e639de42d
VruttiMoradiya999 <vrutimoradiya999@gmail.com>
2026-05-09 19:18:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = require('./backend/server');
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 // The "catchall" handler
-app.get('/*', (req, res) => {
+app.get('/:path*', (req, res) => {
   if (req.path.startsWith('/api')) {
     return res.status(404).send('API route not found');
   }
