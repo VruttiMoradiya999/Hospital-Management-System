68105d31f618b6afa28d06a64a92f759671da7a9
VruttiMoradiya999 <vrutimoradiya999@gmail.com>
2026-05-09 19:13:41 +0530

    Update

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = require('./backend/server');
 
-// Exporting for Vercel
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  if (req.path.startsWith('/api')) {
+    return res.status(404).send('API route not found');
+  }
+  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'));
+});
+
 module.exports = app;
