6f7bc345d00f8e020d22e648fa05d4d55a982ed6
VruttiMoradiya999 <vrutimoradiya999@gmail.com>
2026-05-09 19:16:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const app = require('./backend/server');
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+// The "catchall" handler
+app.get('/*', (req, res) => {
   if (req.path.startsWith('/api')) {
     return res.status(404).send('API route not found');
   }
